a83b4c399e59e72831162e58abdfb97f8ca68071
“chuyang” <511990372@qq.com>
2019-12-02 22:32:03 +0800

    eureka实现服务的注册与发现，ribbon实现负载均衡

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/cy/springcloud/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.cy.springcloud.service;
+
+import com.cy.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
